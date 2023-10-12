dff475a57c9c958dfb4afab38062f3a5ec4da8ed
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-12 13:31:01 +0200

    host fixed

--- global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -1,13 +1,21 @@
 package com.ltp.globalsuperstore;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class StoreController {
 
   @GetMapping("/")
   public String getForm(Model model) {
-    model.addAttribute("categories", Constants.CATEGORIES);
-    return "form";
+      model.addAttribute("categories", Constants.CATEGORIES);
+      return "form";
   }
+
+
+
+
+
+
 }
