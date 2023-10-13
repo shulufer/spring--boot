5c4e2195f8b2081143f10328188e9bc9f521d74e
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-13 12:57:55 +0200

    Item class was created

---
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
@@ -0,0 +1,67 @@
+package com.ltp.globalsuperstore;
+
+import java.util.Date;
+
+public class Item {
+
+  private String category;
+  private String name;
+  private Double price;
+  private Double discount;
+  private Date date;
+
+
+  public Item() {
+  }
+
+  public Item(String category, String name, Double price, Double discount, Date date) {
+    this.category = category;
+    this.name = name;
+    this.price = price;
+    this.discount = discount;
+    this.date = date;
+  }
+
+  public String getCategory() {
+    return this.category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getPrice() {
+    return this.price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public Double getDiscount() {
+    return this.discount;
+  }
+
+  public void setDiscount(Double discount) {
+    this.discount = discount;
+  }
+
+  public Date getDate() {
+    return this.date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+
+
+}
