d6dbd4b6790fa1a4910495ff4c7a36ac9cec9eea
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-19 11:46:13 +0200

    exception handed with spring

---
+++ contacts-controllerAdvice /src/main/java/com/ltp/contacts/exception/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.ltp.contacts.exception;
+
+public class ErrorResponse {
+
+    private String message;
+
+
+  public ErrorResponse(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return this.message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+}
