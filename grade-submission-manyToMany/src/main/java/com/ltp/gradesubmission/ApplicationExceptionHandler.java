2cc1501332cb0036f982c2542a63ad42987ac51e
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-26 11:26:32 +0200

    StudentNotEnrolledException was created

--- grade-submission-manyToMany/src/main/java/com/ltp/gradesubmission/ApplicationExceptionHandler.java
+++ grade-submission-manyToMany/src/main/java/com/ltp/gradesubmission/ApplicationExceptionHandler.java
@@ -19,27 +19,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.ltp.gradesubmission.exception.CourseNotFoundException;
 import com.ltp.gradesubmission.exception.ErrorResponse;
 import com.ltp.gradesubmission.exception.GradeNotFoundException;
+import com.ltp.gradesubmission.exception.StudentNotEnrolledException;
 import com.ltp.gradesubmission.exception.StudentNotFoundException;
 
 
 @ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({CourseNotFoundException.class, GradeNotFoundException.class, StudentNotFoundException.class})
+    @ExceptionHandler({CourseNotFoundException.class, GradeNotFoundException.class, StudentNotFoundException.class, StudentNotEnrolledException.class})
     public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex) {
-        ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getMessage()));  
+        ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getMessage()));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<Object> handleDataAccessException(EmptyResultDataAccessException ex) {
-        ErrorResponse error = new ErrorResponse(Arrays.asList("Cannot delete non-existing resource"));  
+        ErrorResponse error = new ErrorResponse(Arrays.asList("Cannot delete non-existing resource"));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        ErrorResponse error = new ErrorResponse(Arrays.asList("Data Integrity Violation: we cannot process your request."));  
+        ErrorResponse error = new ErrorResponse(Arrays.asList("Data Integrity Violation: we cannot process your request."));
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
--- grade-submission-manyToMany/src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
+++ grade-submission-manyToMany/src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
@@ -6,6 +6,7 @@ import com.ltp.gradesubmission.entity.Course;
 import com.ltp.gradesubmission.entity.Grade;
 import com.ltp.gradesubmission.entity.Student;
 import com.ltp.gradesubmission.exception.GradeNotFoundException;
+import com.ltp.gradesubmission.exception.StudentNotEnrolledException;
 import com.ltp.gradesubmission.repository.CourseRepository;
 import com.ltp.gradesubmission.repository.GradeRepository;
 import com.ltp.gradesubmission.repository.StudentRepository;
@@ -18,12 +19,12 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Service
 public class GradeServiceImpl implements GradeService {
-    
+
     GradeRepository gradeRepository;
     StudentRepository studentRepository;
     CourseRepository courseRepository;
 
-    
+
     @Override
     public Grade getGrade(Long studentId, Long courseId) {
         Optional<Grade> grade = gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
@@ -34,6 +35,7 @@ public class GradeServiceImpl implements GradeService {
     public Grade saveGrade(Grade grade, Long studentId, Long courseId) {
         Student student = StudentServiceImpl.unwrapStudent(studentRepository.findById(studentId), studentId);
         Course course = CourseServiceImpl.unwrapCourse(courseRepository.findById(courseId), courseId);
+        if (!student.getCourses().contains(course)) throw new StudentNotEnrolledException(studentId, courseId);
         grade.setStudent(student);
         grade.setCourse(course);
         return gradeRepository.save(grade);
@@ -66,7 +68,7 @@ public class GradeServiceImpl implements GradeService {
     public List<Grade> getAllGrades() {
         return (List<Grade>) gradeRepository.findAll();
     }
-    
+
     static Grade unwrapGrade(Optional<Grade> entity, Long studentId, Long courseId) {
         if (entity.isPresent()) return entity.get();
         else throw new GradeNotFoundException(studentId, courseId);
